383461f1295b5ab8fd60402f24b3a8baa40ecd83
akyeshi <akyeshi@outlook.com>
2024-04-13 23:26:30 -0400

    playing with index route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
   res.render("index", { title: "Express" });
 });
 
@@ -10,19 +10,20 @@ router.get("/sum/:n1/:n2", function (req, res) {
   console.log(req.params);
   const n1 = parseInt(req.params.n1);
   const n2 = parseInt(req.params.n2);
-  res.send(`The sum of ${n1} and ${n2} is: ${n1 + n2}`);
+  res.send(`Using req.params to grab all the url params
+  to get the sum of ${n1} and ${n2} is: ${n1 + n2}`);
 });
 
+router.get("/send", (req, res) => {
+  res.send("res.send -> hello world");
+});
 
-
-
-
-
-
-
-
-
-
-
+router.get("/json", (req, res) => {
+  res.json({
+    name: "Tom",
+    age: 28,
+    favColor: "Purple",
+  });
+});
 
 module.exports = router;
